463c7c2671308dc7ea470ed3cfa520dde3adf704
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2011-12-04 16:33:30 +0900

    xmonad: Change the key bindings to switch to Xinerama screens.

--- xmonad.hs
+++ xmonad.hs
@@ -131,7 +131,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
     -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
     [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-        | (key, sc) <- zip [xK_w, xK_e] [0..]
+        | (key, sc) <- zip [xK_o, xK_a] [0..]
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
 
